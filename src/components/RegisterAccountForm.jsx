25c74fb0134eecece4211b1c2ba512993b353b7a
Tal Weiss <Tweiss747@gmail.com>
2022-04-19 17:00:44 +0300

    implemented client side error handling on creating new account

--- src/components/RegisterAccountForm.jsx
+++ src/components/RegisterAccountForm.jsx
@@ -9,24 +9,57 @@ export default function RegisterAccountForm({handleClose}){
     const [firstName,setFirstName] = useState('');
     const [lastName,setLastName] = useState('');
 
+    const [hasErrors, setHasErrors] = useState(false);
+
     function closeForm(){
         handleClose();
+    }
+
+    function fieldOnChange(event){
+        switch(event.target.id){
+            case 'email':
+                setUsername(event.target.value);
+                break;
+            case 'first-name':
+                setFirstName(event.target.value);
+                break;
+            case 'last-name':
+                setLastName(event.target.value);
+                break;
+            case 'password':
+                setPassword(event.target.value);
+                break;
+            default:
+                break;
+        }   
+    }
+
+
+    function createAccount(){
+        setHasErrors(false)
+        if(!userName || !firstName || !lastName || !password){
+            setHasErrors(true)
+            return
+        }
+
     }
 
 
     return(
         <>
+            {hasErrors && <p>One or more fields is missing...</p>}
             <div className='form-background'>
                 <div className="form-container">
                     <span className='close-icon' onClick={closeForm} >x</span>
                     <label>Email</label>
-                    <input type='text' placeholder="email" />
+                    <input onChange={fieldOnChange} id='email' type='text' placeholder="email" />
                     <label>First Name</label>
-                    <input type='text' placeholder="first name" />
+                    <input onChange={fieldOnChange} id='first-name' type='text' placeholder="first name" />
                     <label>Last Name</label>
-                    <input type='text' placeholder="last name" />
+                    <input onChange={fieldOnChange} id='last-name' type='text' placeholder="last name" />
                     <label>Password</label>
-                    <input type='password' placeholder="password" />
+                    <input onChange={fieldOnChange} id='password' type='password' placeholder="password" />
+                    <button onClick={createAccount}>Create Account</button>
                 </div>
 
 
