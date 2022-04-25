c3fac86f730dfcd241ff7c1c43b7d9ead5f639a3
Dennisoshak <dennisoshak@gmail.com>
2022-04-25 12:55:19 +0300

    protected route

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Home from "./pages/Home.jsx";
 import Game from "./pages/Game.jsx";
 import Login from "./pages/Login";
 import AuthenticationProvider from "./Contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <AuthenticationProvider>
-
           <Routes>
-            
-            <Route path="/home" element={<Home />} />
-            <Route path="/game" element={<Game />} />
-            <Route path='/' element={<Login />} />
+            <ProtectedRoute
+              Route
+              path="/home"
+              element={<Home />}></ProtectedRoute>
+            <ProtectedRoute
+              Route
+              path="/game"
+              element={<Game />}></ProtectedRoute>
+            <Route path="/" element={<Login />} />
           </Routes>
         </AuthenticationProvider>
-
       </BrowserRouter>
     </div>
   );
